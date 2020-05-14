e56ddd2b7b42c7ebe323b6a971ef1df9d9d02898
hotheadhacker <isalmanqureshi@gmail.com>
2020-05-14 22:12:16 +0530

    :rocket: Project Completed & operational :green_heart:

--- location.py
+++ location.py
@@ -8,6 +8,8 @@
 import config
 import smtplib
 import emailList
+import re
+from urllib.parse import urlencode
 from twitter import *
 
 # -----------------------------------------------------------------------
@@ -32,6 +34,14 @@ twitter = Twitter(auth=OAuth(config.access_key,
 # -----------------------------------------------------------------------
 results = twitter.trends.place(_id=2295387)
 
+# gets Matches
+
+hit = ['Kashmir', 'srinagar', 'pulwama', 'jammu', 'pakistan',
+       'encounter', 'terrorist', 'attack', 'freedom', 'lockdown', 'anantnag', 'shopian', 'kulgam', 'sopore', 'khan', 'muslims', 'islam', 'nirav', 'khan']
+
+res = []
+
+
 mailResult = ""
 print("Srinagar - Kashmir Trends")
 i = 0
@@ -39,56 +49,79 @@ i = 0
 for location in results:
     for trend in location["trends"]:
         i = i+1
-        print(i)
-        print(" - %s" % trend["name"])
-        print(" URL:  %s" % trend["url"])
-        mailResult += str(i) + ") " + \
-            trend["name"] + "\n" + trend["url"] + "\n \n"
+        # print(i)
+        # print(" - %s" % trend["name"])
+        # print(" URL:  %s" % trend["url"])
+        # mailResult += str(i) + ") " + \
+        #     trend["name"] + "\n" + trend["url"] + "\n \n"
+        res.append(trend["name"])
 
-        if i == 10:
-            break
+        # if i == 10:
+        #     break
 
-# query = twitter.search.tweets(q="salmanually")
-# How long did this query take?
-# -----------------------------------------------------------------------
-# print("Search complete (%.3f seconds)" %
-#       (query["search_metadata"]["completed_in"]))
+        # query = twitter.search.tweets(q="salmanually")
+        # How long did this query take?
+        # -----------------------------------------------------------------------
+        # print("Search complete (%.3f seconds)" %
+        #       (query["search_metadata"]["completed_in"]))
 
-# -----------------------------------------------------------------------
-# Loop through each of the results, and print its content.
-# -----------------------------------------------------------------------
-# for result in query["statuses"]:
-#     print("(%s) @%s %s" % (result["created_at"],
-#                            result["user"]["screen_name"], result["text"]))
+        # -----------------------------------------------------------------------
+        # Loop through each of the results, and print its content.
+        # -----------------------------------------------------------------------
+        # for result in query["statuses"]:
+        #     print("(%s) @%s %s" % (result["created_at"],
+        #                            result["user"]["screen_name"], result["text"]))
 
-# send Mail
-TO = emailList.subscribers
-# print(emailList.subscribers)
-SUBJECT = 'Trends For Kashmir'
-TEXT = 'Here are Top trends from Kashmir: \n'
+        # send Mail
+print("Matched With: ")
 
-TEXT += mailResult
+matches = []
+for trends in hit:
+    pattern = re.compile(trends, re.IGNORECASE)
+    container = [x for x in res if pattern.search(x)]
+    if(len(container) > 0):
+        for i in container:
+            payload = {'q': i}
+            encode = urlencode(payload)
+            url = "https://twitter.com/search?"+encode
+            print(url)
+            wrapper = i + " \n " + url + " \n "
+            matches.append(wrapper)
 
-TEXT += "\n This is trial based project Feedbacks & Suggestions are Most Welcome @ https://www.twitter.com/salmanually \n _____________ \n Want To add your friend or another account? just DM email to https://www.twitter.com/salmanually \n If you want to unsubscribe \n email to: contact@salmanually.com \n or \n tweet/DM: https://www.twitter.com/salmanually"
+print(matches)
 
-# Gmail Sign In
-gmail_sender = 'kmrtrends@gmail.com'
-gmail_passwd = config.password
+if(len(matches) > 0):
+    TO = emailList.subscribers
+    # print(emailList.subscribers)
+    SUBJECT = 'Trends For Kashmir'
+    TEXT = 'Here are Top trends from Kashmir: \n'
 
-server = smtplib.SMTP('smtp.gmail.com', 587)
-server.ehlo()
-server.starttls()
-server.login(gmail_sender, gmail_passwd)
+    TEXT += str(", ".join(matches))
 
-BODY = '\r\n'.join(['To: %s' % TO,
-                    'From: %s' % gmail_sender,
-                    'Subject: %s' % SUBJECT,
-                    '', TEXT])
+    print(TEXT)
 
-try:
-    server.sendmail(gmail_sender, TO.split(","), BODY)
-    print('email sent')
-except:
-    print('error sending mail')
+    TEXT += "\n This is trial based project Feedbacks & Suggestions are Most Welcome @ https://www.twitter.com/salmanually \n _____________ \n Want To add your friend or another account? just DM email to https://www.twitter.com/salmanually \n If you want to unsubscribe \n email to: contact@salmanually.com \n or \n tweet/DM: https://www.twitter.com/salmanually"
 
-server.quit()
+#     Gmail Sign In
+    gmail_sender = 'kmrtrends@gmail.com'
+    gmail_passwd = config.password
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login(gmail_sender, gmail_passwd)
+
+    BODY = '\r\n'.join(['To: %s' % TO,
+                        'From: %s' % gmail_sender,
+                        'Subject: %s' % SUBJECT,
+                        '', TEXT])
+
+    try:
+        server.sendmail(gmail_sender, TO.split(","), BODY)
+        print('email sent')
+    except:
+        print('error sending mail')
+
+        server.quit()
+else:
+    print("Not matching Trends For Now!")
--- test.py
+++ test.py
@@ -1,3 +1,14 @@
-name = "Salman"
-name += "Hamid"
-print(name)
+# import re
+# hit = ['Kashmir', 'kashmirsrinagar', 'pulwama', 'jammu kashmir', 'pakistan',
+#        'encounter', 'terrorist', 'attack', 'freedom', 'lockdown', 'anantnag', 'shopian', 'kulgam', 'sopore', '#kashmir']
+# eg = 'kashmir'
+# pattern = re.compile(eg, re.IGNORECASE)
+# matches = [x for x in hit if pattern.search(x)]
+
+# print(matches)
+
+from urllib.parse import urlencode
+
+payload = {'q': '#administrator', 'password': 'xyz'}
+result = urlencode('#administrator')
+print(result)
